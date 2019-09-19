2c7cd1d1f0876ce8b7e3e0d7ea0e93847cff2d91
马凯 <makai@fit2cloud.com>
2019-09-19 14:14:38 +0800

    修改 build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,4 @@ docker login -u north-developer -p Calong@2015 registry.fit2cloud.com
 
 docker build -t registry.fit2cloud.com/north/test:20190918 .
 docker push registry.fit2cloud.com/north/test:20190918
-docker images | grep test
-docker logout
+docker logout registry.fit2cloud.com
