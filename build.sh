b50352c64317e2042a1e80387be1903bc25d9fa3
马凯 <makai@fit2cloud.com>
2019-09-19 11:55:33 +0800

    修改build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 mvn clean package -Dmaven.test.skip=true
 
-docker login -u north-developer -p Calong@2015
+docker login -u north-developer -p Calong@2015 registry.fit2cloud.com
 
 docker build -t registry.fit2cloud.com/north/test:20190918 .
 docker push registry.fit2cloud.com/north/test:20190918
