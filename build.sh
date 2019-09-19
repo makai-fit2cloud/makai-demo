71821da0c214df5498fa19e46f47b87e95b885e1
马凯 <makai@fit2cloud.com>
2019-09-19 14:11:28 +0800

    输出镜像信息

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ docker login -u north-developer -p Calong@2015 registry.fit2cloud.com
 
 docker build -t registry.fit2cloud.com/north/test:20190918 .
 docker push registry.fit2cloud.com/north/test:20190918
-
+docker images | grep test
 docker logout
