cbae788678967851f2942d95b8d6286877d0eb6d
马凯 <makai@fit2cloud.com>
2019-09-19 11:45:05 +0800

    no message

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 mvn clean package -Dmaven.test.skip=true
 
+docker login -u north-developer -p Calong@2015
+
 docker build -t registry.fit2cloud.com/north/test:20190918 .
 docker push registry.fit2cloud.com/north/test:20190918
+
+docker logout
